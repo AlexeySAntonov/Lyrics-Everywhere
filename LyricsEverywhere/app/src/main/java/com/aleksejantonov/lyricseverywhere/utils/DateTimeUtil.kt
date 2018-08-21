5c1e67687a2cb56cb7b0f46608b3b54ec99492cf
Alexey Antonov <alex.antonov150@gmail.com>
2018-08-21 23:19:50 +0300

    Bugfix/Night time.

--- LyricsEverywhere/app/src/main/java/com/aleksejantonov/lyricseverywhere/utils/DateTimeUtil.kt
+++ LyricsEverywhere/app/src/main/java/com/aleksejantonov/lyricseverywhere/utils/DateTimeUtil.kt
@@ -7,7 +7,7 @@ object DateTimeUtil {
   fun isNightModeNecessary(): Boolean {
     val calendar = Calendar.getInstance()
     val hour = calendar.get(Calendar.HOUR_OF_DAY)
-    if (hour in 0..6 && hour in 18..23) return true
+    if (hour in 0..6 || hour in 20..23) return true
     return false
   }
 }
