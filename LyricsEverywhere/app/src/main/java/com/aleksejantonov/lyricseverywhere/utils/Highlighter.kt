750f46fe32483eb07f11182c3299b827670b3bbc
Alexey Antonov <alex.antonov150@gmail.com>
2018-10-27 15:18:16 +0300

    refactor/highlight optimize.

--- LyricsEverywhere/app/src/main/java/com/aleksejantonov/lyricseverywhere/utils/Highlighter.kt
+++ LyricsEverywhere/app/src/main/java/com/aleksejantonov/lyricseverywhere/utils/Highlighter.kt
@@ -2,21 +2,15 @@ package com.aleksejantonov.lyricseverywhere.utils
 
 import android.content.Context
 import android.text.SpannableString
-import android.text.TextUtils
 import android.text.style.TextAppearanceSpan
 import com.aleksejantonov.lyricseverywhere.R
 
-fun String.highlight(query: String, context: Context): SpannableString? {
-  indexOfSearchQuery(this, query).let {
-    if (it == -1) return null
-    return SpannableString(this).apply {
-      setSpan(TextAppearanceSpan(context, R.style.searchTextHighlight), it, it + query.length, 0)
+fun String.highlight(query: String, context: Context): SpannableString? =
+    indexOfSearchQuery(this, query)?.let {
+      if (it != -1) SpannableString(this).apply { setSpan(TextAppearanceSpan(context, R.style.searchTextHighlight), it, it + query.length, 0) }
+      else null
     }
-  }
-}
 
-private fun indexOfSearchQuery(preparingString: String, query: String): Int {
-  return if (!TextUtils.isEmpty(query)) {
-    preparingString.toLowerCase().indexOf(query.toLowerCase())
-  } else -1
-}
+
+private fun indexOfSearchQuery(preparingString: String, query: String?): Int? =
+    query?.let { preparingString.indexOf(it, ignoreCase = true) }
